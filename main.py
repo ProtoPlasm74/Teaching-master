2ea9ac587daee6d34d99a0c6b793d7d3f21286f2
Joey Rockwood <jarockwood@outlook.com>
2016-11-14 13:47:40 -0500

    Joey: Created page2.html. Added Button to index.html. Deleted carousel of images in index.

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route('/page2')
+def hello_world():
+    return render_template("page2.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
