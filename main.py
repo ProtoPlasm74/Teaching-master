4d510c12d86ceffecbfae31ee128f27d1c2fcc4c
ProtoPlasm74 <zm.85775@gmail.com>
2016-11-16 13:03:26 -0500

    Added extra pages to flask

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ def hello_world():
 def hello_world2():
     return render_template("pg2.html")
 
-@app.route('/page2')
-def hello_world():
-    return render_template("page2.html")
+@app.route('/Results')
+def hello_world3():
+    return render_template("Results.html")
+
+@app.route('/index_return')
+def hello_world4():
+    return render_template("index_return.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
