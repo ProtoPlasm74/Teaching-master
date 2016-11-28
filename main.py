62d06e7e8bc26da54e2f274e06184b387f383889
ProtoPlasm74 <zm.85775@gmail.com>
2016-11-28 13:16:33 -0500

    made a default host

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ def index_return():
     return render_template("index_return.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0' debug=True)
---
+++ watson_converter.py
@@ -0,0 +1,26 @@
+import json
+from watson_developer_cloud import DocumentConversionV1
+
+document_conversion = DocumentConversionV1(
+  url = "https://gateway.watsonplatform.net/document-conversion/api",
+  password='3QKUJFXDg1GK',
+  username='6898f353-4f93-4a45-ab5f-7e0c73dd252d',
+  version='2015-12-15'
+)
+
+config = {
+  'conversion_target': 'ANSWER_UNITS',
+  # Use a custom configuration.
+  'word': {
+    'heading': {
+      'fonts': [
+        {'level': 1, 'min_size': 24},
+        {'level': 2, 'min_size': 16, 'max_size': 24}
+      ]
+    }
+  }
+}
+
+with open(('/Users/zacmorgan/Downloads/Managing for Success.pdf'), 'r') as document:
+  response = document_conversion.convert_document(document=document, config=config)
+  print(json.dumps(response, indent=2))
