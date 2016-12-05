e0b8f5294f4ed51167efdec0e5b2e2de29edd7a6
ProtoPlasm74 <zacmorgan@Mac-Pro.local>
2016-12-04 23:06:27 -0500

    added os and some other file upload stuff

--- main.py
+++ main.py
@@ -10,9 +10,26 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'pdf'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
     return render_template("index.html")
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
 	
 @app.route('/pg2')
 def progress_bar():
