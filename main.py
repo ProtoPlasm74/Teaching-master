bca7d3a36a2bb745ea8363d6a116592ccde090ff
Miguel Martinez <Miguel Martinez>
2016-12-07 16:31:47 -0500

    commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import os
 from werkzeug.utils import secure_filename
-from engine.converter import ConvertDoc
+#from engine.converter import ConvertDoc
 import json
 
 
@@ -17,25 +17,10 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-            return render_template("index.html")
-
-    return render_template("index.html")
+	if request.method == 'POST':
+		f = request.files['file']
+		f.save(f.filename)
+		# process is the function that i'm sending the file to, which in this case is a .xlsx file
 
 
 @app.route('/uploads/<filename>')
@@ -56,4 +41,4 @@ def index_return():
     return render_template("index_return.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0' debug=True)
+    app.run(host='127.0.0.1', debug=True)
